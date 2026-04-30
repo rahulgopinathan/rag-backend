edfe701b0e27a9ab37d0b395b5e7ab761ddab351
Rahul Gopinathan <rahulthettikulam@gmail.com>
2026-04-30 16:37:38 +0100

    Load cors FE url from env

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", // your frontend URL
   }),
 );
 
